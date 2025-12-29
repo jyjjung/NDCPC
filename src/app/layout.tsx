dc3d84fd9197acfb63508acc202c803c51636e65
John Jung <yejoon7154@gmail.com>
2025-12-29 13:06:10 +0000

    The calendar looks broken in the roster form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import 'react-day-picker/dist/style.css';
 import { cn } from '@/lib/utils';
 import { AdminProvider } from '@/context/AdminProvider';
 import { Header } from '@/components/Header';
